9be588c75074035d52139b63e4040d82b7164e3b
Gabriel-Philot <bilphilot@gmail.com>
2023-02-15 20:07:39 -0300

    lambda f fix

--- etl/lambda_function.py
+++ etl/lambda_function.py
@@ -11,7 +11,7 @@ def handler(event, context):
                 ServiceRole='EMR_DefaultRole',
                 JobFlowRole='EMR_EC2_DefaultRole',
                 VisibleToAllUsers=True,
-                LogUri='s3:/datalake-edc-mod1-studycases-prod/emr-logs/',
+                LogUri='s3://datalake-edc-mod1-studycases-prod/emr-logs/',
                 ReleaseLabel='emr-6.3.0',
                 Instances={
                     'InstanceGroups': [
