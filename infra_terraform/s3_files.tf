b6e96ee6c9e4aef735e715fd098fb8995abbe94c
Gabriel-Philot <bilphilot@gmail.com>
2023-02-16 12:22:28 -0300

    new day please run job

--- infra_terraform/s3_files.tf
+++ infra_terraform/s3_files.tf
@@ -3,7 +3,7 @@
 # File spark job for emr
 resource "aws_s3_object" "codigo_spark_emr" {
   bucket = aws_s3_bucket.datalake.id #Referêrncia do datalake criado
-  key    = "emr-code/pyspark/delta_spark_01_insert.py"
+  key    = "emr-code/pyspark/pyspark_01_insert.py"
   source = "../etl/delta_spark_01_insert.py"          # Arquivo com o job spark que vai
   etag   = filemd5("../etl/delta_spark_01_insert.py") # Define qual é o objeto de parâmetro
 }
@@ -11,7 +11,7 @@ resource "aws_s3_object" "codigo_spark_emr" {
 # File spark job for glue
 resource "aws_s3_object" "codigo_spark_glue" {
   bucket = aws_s3_bucket.datalake.id #Referêrncia do datalake criado
-  key    = "emr-code/pyspark/delta_spark_uspert_02.py"
+  key    = "emr-code/pyspark/pyspark_uspert_02.py"
   source = "../etl/delta_spark_uspert_02.py"          # Arquivo com o job spark que vai
   etag   = filemd5("../etl/delta_spark_uspert_02.py") # Define qual é o objeto de parâmetro
 }
